e7431778a77e923e4e02eb661151b832f0889b12
Evan Brown <evandbrown@gmail.com>
2013-04-09 16:57:17 -0700

    Code cleanup

--- background.js
+++ background.js
@@ -1,23 +1,20 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Called when a message is passed.  We assume that the content script
-// wants to show the page action.
+// Executed when a request is passed from a contentscript, which
+// will pass a mappings object to this background script
 function onRequest(request, sender, sendResponse) {
   // Show the page action for the tab that the sender (content script)
   // was on.
   chrome.pageAction.show(sender.tab.id);
 
+	// Set the mappings object to the value passed from the contentscript
 	mappings = request;
-	
-	console.log(mappings);
 
-  // Return nothing to let the connection be cleaned up.
+  // Return an empty object
   sendResponse({});
 };
 
 // Listen for the content script to send a message to the background page.
 chrome.extension.onRequest.addListener(onRequest);
 
+// Initialize an empty mappings object. This object
+// will be retrieved by the popup
 mappings = {};
--- contentscript.js
+++ contentscript.js
@@ -1,29 +1,35 @@
-/** This script is executed when a user visits an http(s)://aws.amazon.com/amis/* URL **/
-//Stub out a Mappings object
-mappings = 
-{
-	"Mappings" : {
-		"RegionMap" : {
+// This script is executed when a user visits an http(s)://aws.amazon.com/amis/* URL
 
+// Build an object of region->AMI mappings that's compatible
+// with the CloudFormation mapping structure
+function getMappings() {
+	//Stub out a Mappings object
+	mappings = 
+	{
+		"Mappings" : {
+			"RegionMap" : {
+
+			}
 		}
-	}
-};
+	};
 
-// Build regular expressions to extract region
-// and AMI parameters from URL
-regionRegex = /\?region=(.*)#/;
-amiRegex = /launchAmi=(ami-[a-h0-9]+)/;
+	// Build regular expressions to extract region
+	// and AMI parameters from URL
+	regionRegex = /\?region=(.*)#/;
+	amiRegex = /launchAmi=(ami-[a-h0-9]+)/;
 
-// Get each item from the 'Launch AMI' dropdown
-$('.ami_launch_dropdown li a').each(function() {
-	// Get and parse HREF for region and AMI ID
-	href = $(this).attr('href');
-	region = regionRegex.exec(href)[1];
-	ami = { "AMI" : amiRegex.exec(href)[1] };
+	// Get each item from the 'Launch AMI' dropdown
+	$('.ami_launch_dropdown li a').each(function() {
+		// Get and parse HREF for region and AMI ID
+		href = $(this).attr('href');
+		region = regionRegex.exec(href)[1];
+		ami = { "AMI" : amiRegex.exec(href)[1] };
 
-	// Append a mapping for this AMI and Region
-	mappings.Mappings.RegionMap[region] = ami;	
-});
-
+		// Append a mapping for this AMI and Region
+		mappings.Mappings.RegionMap[region] = ami;	
+	});
+	
+	return mappings;
+}
 // Notify the background script and pass the mappings object
-chrome.extension.sendRequest(mappings, function(response) { console.log("Response received from background pages.") } );
+chrome.extension.sendRequest(getMappings(), function(response) {} );
