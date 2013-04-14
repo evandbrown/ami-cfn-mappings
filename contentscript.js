aeb4141362a28bac62da25148ce0947fceff4c37
Evan Brown <evandbrown@gmail.com>
2013-04-13 21:40:54 -0700

    Added support for https://aws.amazon.com/amazon-linux-ami/, including drop-down to select different AMI types (e.g., 32 bit, storage device type, or virt type)

--- contentscript.js
+++ contentscript.js
@@ -1,35 +1,115 @@
-// This script is executed when a user visits an http(s)://aws.amazon.com/amis/* URL
+/**
+  * This content script will be executed in the DOM context
+  * of the URLs defined in the manifest. Bsaed on the URL, the
+  * script determines which jQuery-powered screen-scraping
+  * algorithm to use to generate an mappings object, then
+  * fires an event that is received by background.js, which
+  * makes the mappings object available to the popup.
+  **/
+
+// Build a mappings object given a name, description,
+// and array of region->ami items
+function getMappingsObject(mappingName, mappingDesc, regionToAmiList) {
+	var mappings = {
+		description : mappingDesc,
+		list : regionToAmiList,
+		name : mappingName
+	}
+	
+	return mappings;
+}
 
 // Build an object of region->AMI mappings that's compatible
 // with the CloudFormation mapping structure
-function getMappings() {
-	//Stub out a Mappings object
-	mappings = 
-	{
-		"Mappings" : {
-			"AWSRegionToAMI" : {
+function getMappingsFromAmisPage() {
+	// List for holding region->ami pairs
+	var regionToAmi = [];
 
-			}
-		}
-	};
-
-	// Build regular expressions to extract region
-	// and AMI parameters from URL
-	regionRegex = /\?region=(.*)#/;
-	amiRegex = /launchAmi=(ami-[a-h0-9]+)/;
-
+	var desc, href;
+	
 	// Get each item from the 'Launch AMI' dropdown
 	$('.ami_launch_dropdown li a').each(function() {
 		// Get and parse HREF for region and AMI ID
 		href = $(this).attr('href');
-		region = regionRegex.exec(href)[1];
-		ami = { "AMI" : amiRegex.exec(href)[1] };
-
+		desc = $('.title').text();
+		
 		// Append a mapping for this AMI and Region
-		mappings.Mappings.AWSRegionToAMI[region] = ami;	
+		regionToAmi.push( { region : getRegionFromHref(href), ami : getAmiIdFromHref(href) } );
+	});
+	
+	return getMappingsObject("AWSRegionToAMI", desc, regionToAmi);
+}
+
+// Build an object of region->AMI mappings from the
+// Amazon Linux landing page
+function getMappingsFromALinuxLandingPage() {
+	//Stub out a Mappings object and other vars
+	var mappings = [],
+			regionToAmi = [],
+			name = "AWSRegionToAMI",
+			href,
+			desc,
+			offset,
+			amiTd;
+
+	// Locate the AMI table
+	var amiTable = $('h1:contains("Amazon Linux AMI IDs")').next();
+
+	// Get each column, which indicates Storage/Arch/VirtType of an AMI. 
+	// Each column will correspond to an AMI
+	$(amiTable).find('tbody > tr:first > td').each(function(col, td) {
+		// Rest the array of region->ami items
+		regionToAmi = [];
+		
+		// Ignore the first col
+		if(0 != col) { 
+			desc = $(td).text();
+			
+			// Iterate through each row for for this column
+			$(amiTable).find('tbody > tr').each(function(row, tr) {
+				// Ignore the first row
+				if(0 != row) {
+					offset = col + 1;
+					amiTd = $(tr).find('td:nth-child('+offset+')');
+					if($(amiTd).children().size() > 0) {
+						href = $(amiTd).children('a').attr('href');
+						
+						// Append a mapping for this AMI and Region
+						regionToAmi.push( { region : getRegionFromHref(href), ami : getAmiIdFromHref(href) } );
+					}
+				}
+			});
+			
+			// Add mapping for this AMI to collection
+			mappings.push(getMappingsObject(name, desc, regionToAmi));
+		}
 	});
 	
 	return mappings;
 }
-// Notify the background script and pass the mappings object
-chrome.extension.sendRequest(getMappings(), function(response) {} );
+
+// Return the region code (e.g., us-east-1) from the href
+// of a link on aws.amazon.com
+function getRegionFromHref(href) {
+	var regionRegex = /\?region=(.*)#/;
+	return regionRegex.exec(href)[1];
+}
+
+// Return the AMI ID from the href of a link on aws.amazon.com
+function getAmiIdFromHref(href) {
+	amiRegex = /launchAmi=(ami-[a-h0-9]+)/;
+	return amiRegex.exec(href)[1];
+}
+
+// GLOBALS
+var mappings;
+
+// Use a different parsing mechanism depending on the page we're on:
+// http(s)://aws.amazon.com/amazon-linux-ami
+if(/^https?:\/\/aws.amazon.com\/amazon-linux-ami\/?$/.exec(document.location.href) != null) {
+	mappings = getMappingsFromALinuxLandingPage();
+} else { // http(s)://aws.amazon.com/amis/*
+	mappings = getMappingsFromAmisPage();
+}
+
+chrome.extension.sendRequest(mappings, function(response) {} );
--- popup.js
+++ popup.js
@@ -1,8 +1,71 @@
 $(document).ready(function() {
-	// Get Mappings JSON from backgrond page and stringify it
-	mappings = JSON.stringify(chrome.extension.getBackgroundPage().mappings, null, 4);
+	// Get the mappings object from the background page
+	mappings = chrome.extension.getBackgroundPage().mappings;
 	
-	// Get rid of the opening and closing braces
-	mappings = mappings.substring(1, mappings.length-1);
-	$('#mappings').text(mappings);
+	// Format the mappings for output
+	mappings = formatMappings(mappings);
+	
+	// Get each mapping and draw in the popup
+	$(mappings).each(function(key, value) {
+		$('#mappings')
+			.append($("<option></option")
+			.attr("value", key)
+			.text(value.description));
+	});
+	
+	// Add an onchange event to the dropdown
+	$('#mappings').change(function() {
+		var selected = $('#mappings option:selected').val();
+		$('#mappingCfn').text(mappings[selected].cfn);
+	});
+	
+	// Trigger the onchange event
+	$('#mappings').change();
 });
+
+// Format the mappings object passed from the content script
+// to a format suitable for display
+function formatMappings(mappings) {
+	// If mappings isn't an array, make it one (allows us to support)
+	// providing multiple mapping entries when there different AMI types
+	// available (i.e., ebs, s3, hvm, pv, 32, 64, etc)
+	if(! $.isArray(mappings)) {
+		mappings = new Array(mappings);
+	}
+	
+	var mappingCfnOutput,
+			mappingOutput = [];
+	
+	// Itereate each mapping entry
+	$(mappings).each(function(key, mapping) {		
+		// Convert list of mappings to CFN format
+		mappingCfnOutput = {
+			Mappings : { }
+		}
+	
+		// Set name for mapping
+		mappingCfnOutput.Mappings[mapping.name] = {};
+	
+		var region, ami;
+	
+		// Add each region->AMI item
+		$(mapping.list).each(function(key, regionToAmiEntry) {
+			ami = regionToAmiEntry.ami;
+			region = regionToAmiEntry.region;
+			mappingCfnOutput.Mappings[mapping.name][region] = { "AMI" : ami };
+		});
+	
+		// Get Mappings JSON from backgrond page and stringify it
+		var mappingCfnOutput = JSON.stringify(mappingCfnOutput, null, 4);
+		var description = mapping.description;
+	
+		// Get rid of the opening and closing braces
+		mappingCfnOutput = mappingCfnOutput.substring(1, mappingCfnOutput.length-1);
+		
+		mappingOutput.push( { description : mapping.description, cfn : mappingCfnOutput } );
+	});
+	
+	return mappingOutput;
+}
+
+var mappings;
